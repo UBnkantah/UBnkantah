62ae6b6e416544e1c71a64f037320cd2d7ebf7e8
UBnkantah <ubongnkantahuwem@gmail.com>
2024-03-21 10:06:20 +0000

    change profile pics

--- src/pages/HomePg/HomePg.tsx
+++ src/pages/HomePg/HomePg.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { FaArrowRightLong } from "react-icons/fa6"
-import Ubj from "../../assets/ubj.jpg";
+import Ubj from "../../assets/homeimg.svg";
 import Typewriter from "typewriter-effect";
 
 
